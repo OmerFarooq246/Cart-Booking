44fd9884a315851848ff304841f937abe90ce95d
Omer Farooq <ofarooq.bscs21seecs@seecs.edu.pk>
2025-03-01 18:53:26 +0500

    removed single threading

--- auto.py
+++ auto.py
@@ -1,17 +1,26 @@
-from concurrent.futures import ThreadPoolExecutor
+# from concurrent.futures import ThreadPoolExecutor
 from Flow import Flow
 import time
 
 scan_msg = "Salam Alykom\nI would like to book a Cart from Jawlah"
 flow = Flow(scan_msg)
 
-with ThreadPoolExecutor() as executor:
-    while True:
-        try:
-            e1 = executor.submit(flow.handle_conv_flow)
-            print(e1)
-            time.sleep(3)
-        except Exception:
-            if Exception == KeyboardInterrupt:
-                flow.client.close()
-                print("Stopping execution...")
+while True:
+    try:
+        flow.handle_conv_flow()
+        time.sleep(3)
+    except Exception:
+        if Exception == KeyboardInterrupt:
+            flow.client.close()
+            print("Stopping execution...")
+
+# with ThreadPoolExecutor() as executor:
+#     while True:
+#         try:
+#             e1 = executor.submit(flow.handle_conv_flow)
+#             print(e1)
+#             time.sleep(3)
+#         except Exception:
+#             if Exception == KeyboardInterrupt:
+#                 flow.client.close()
+#                 print("Stopping execution...")
